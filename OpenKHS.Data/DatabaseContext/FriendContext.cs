4e2887a7969a39ea18cba361c5076d816c2b2f04
savaged <david@savaged.info>
2018-04-11 09:59:21 +0100

    Just setting up how the ORM will work

---
+++ OpenKHS.Data/DatabaseContext/FriendContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using OpenKHS.Models;
+
+namespace OpenKHS.Data.DatabaseContext
+{
+    public class FriendContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // TODO move the hard-coded connection to app.config
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\mssqllocaldb; Database = OpenKHS; Trusted_Connection = True;");
+        }
+    }
+}
