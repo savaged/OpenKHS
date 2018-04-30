4fb71d5def7d861935d5b60753892917e107e261
savaged <david@savaged.info>
2018-04-30 14:41:11 +0100

    Working on flagging overloading

---
+++ OpenKHS.Controls/PotentiallyOverloadedFlag.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using OpenKHS.Interfaces;
+
+namespace OpenKHS.Controls
+{
+    public partial class PotentiallyOverloadedFlag
+    {
+        public PotentiallyOverloadedFlag()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext != null)
+            {
+                var friend = (IFriend)DataContext;
+                if (friend.IsPotentiallyOverloaded)
+                {
+                    TxtFlag.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    TxtFlag.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                TxtFlag.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
