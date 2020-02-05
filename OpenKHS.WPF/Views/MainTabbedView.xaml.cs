55dfa0fd912f5581080ebb4a7d23463845c89fee
David Savage <savagedavid@gmail.com>
2020-02-05 09:09:14 +0000

    groundwork for WPF

---
+++ OpenKHS.WPF/Views/MainTabbedView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace OpenKHS.WPF.Views
+{
+    public partial class MainTabbedView
+    {
+        public MainTabbedView()
+        {
+            InitializeComponent();
+        }
+    }
+}
