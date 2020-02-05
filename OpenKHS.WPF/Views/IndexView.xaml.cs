9dfa7b47427a58cd740ca9475ddf74048b916284
David Savage <savagedavid@gmail.com>
2020-02-05 13:47:40 +0000

    groundwork for WPF

---
+++ OpenKHS.WPF/Views/IndexView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace OpenKHS.WPF.Views
+{
+    public partial class IndexView
+    {
+        public IndexView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OpenKHS.WPF/Views/SelectedItemView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace OpenKHS.WPF.Views
+{
+    public partial class SelectedItemView
+    {
+        public SelectedItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
